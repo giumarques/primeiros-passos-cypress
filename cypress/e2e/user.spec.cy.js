11932c46614ce82c172cd24e8ec83ee35e640bb1
Giuliana <giuliana@MacBook-Air.local>
2025-10-08 14:34:26 -0300

    added Page Object

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,15 +1,18 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js' 
+import MenuPage from '../pages/menuPage.js'
+
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+
+
 
 describe('Orange HRM tests spec', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
@@ -20,44 +23,44 @@ describe('Orange HRM tests spec', () => {
     selectOption: ".oxd-radio-wrapper"
     
   }
+
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('John')
-    cy.get(selectorsList.lastNameField).clear().type('Smith')
-    cy.get(selectorsList.genericField).eq(3).clear().type('Employer')
-    cy.get(selectorsList.genericField).eq(4).clear().type('Random Id')
-    cy.get(selectorsList.genericField).eq(5).clear().type('DVlic')
-    cy.get(selectorsList.genericField).eq(6).clear().type('2030-01-01')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.arrowButton).eq(0).click()
-    cy.get('.oxd-select-option').contains('Belgian').click()
-    cy.get(selectorsList.arrowButton).eq(1).click()
-    cy.get('.oxd-select-option').contains('Married').click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('2040-01-01')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get('.oxd-radio-wrapper').contains('Female').click()
-    cy.get(selectorsList.buttonSubmit).eq(0).click()
-    cy.get('body').should('contain', "Successfully Updated")
-    cy.get('.oxd-toast-close')
-    cy.get(selectorsList.arrowButton).eq(2).click()
-    cy.get('.oxd-select-option').contains('B+').click()
-    cy.get(selectorsList.arrowButton).eq(3).click()
-    cy.get('.oxd-select-option').contains('Yes').click()
-    cy.get(selectorsList.genericField).eq(9).clear().type('BloodTest')
-    cy.get(selectorsList.buttonSubmit).eq(1).click()
-    cy.get('body').should('contain', "Successfully Updated")
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    dashboardPage.checkDashboardPage()
+    menuPage.accessMyInfo()
+
+
+    // cy.get(selectorsList.firstNameField).clear().type('John')
+    //cy.get(selectorsList.lastNameField).clear().type('Smith')
+    //cy.get(selectorsList.genericField).eq(3).clear().type('Employer')
+    //cy.get(selectorsList.genericField).eq(4).clear().type('Random Id')
+    //cy.get(selectorsList.genericField).eq(5).clear().type('DVlic')
+    //cy.get(selectorsList.genericField).eq(6).clear().type('2030-01-01')
+    //cy.get(selectorsList.dateCloseButton).click()
+    //cy.get(selectorsList.arrowButton).eq(0).click()
+    //cy.get('.oxd-select-option').contains('Belgian').click()
+    //cy.get(selectorsList.arrowButton).eq(1).click()
+    //cy.get('.oxd-select-option').contains('Married').click()
+    //cy.get(selectorsList.genericField).eq(8).clear().type('2040-01-01')
+    //cy.get(selectorsList.dateCloseButton).click()
+    //cy.get('.oxd-radio-wrapper').contains('Female').click()
+   // cy.get(selectorsList.buttonSubmit).eq(0).click()
+   // cy.get('body').should('contain', "Successfully Updated")
+    //cy.get('.oxd-toast-close')
+   // cy.get(selectorsList.arrowButton).eq(2).click()
+    //cy.get('.oxd-select-option').contains('B+').click()
+    //cy.get(selectorsList.arrowButton).eq(3).click()
+    //cy.get('.oxd-select-option').contains('Yes').click()
+    //cy.get(selectorsList.genericField).eq(9).clear().type('BloodTest')
+    //cy.get(selectorsList.buttonSubmit).eq(1).click()
+    //cy.get('body').should('contain', "Successfully Updated")
 
 
 
    })
 
-  it('Login - Fail', () => {
+  it.skip('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
---
+++ cypress/pages/dashboardPage.js
@@ -0,0 +1,40 @@
+class DashboardPage {
+
+    selectorsList() {
+        const selectors = {
+            dashboardGrid: ".orangehrm-dashboard-grid",
+        }
+
+        return selectors
+    }
+
+    checkDashboardPage() {
+        cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+        cy.get(this.selectorsList().dashboardGrid).should('be.visible')
+    }
+}
+
+export default DashboardPage
+
+
+//Alternative
+
+// class DashboardPage {
+
+//             dashboardGrid = ".orangehrm-dashboard-grid"
+//             myInfoButton = "[href='/web/index.php/pim/viewMyDetails']"
+
+    
+//     accessDashboardPage() {
+//             cy.visit('/web/index.php/dashboard/index')
+        
+// }
+//     checkGrid() {
+//             cy.get(this.dashboardGrid)
+//              cy.get(this.myInfoButton).click()
+// }
+
+
+// }
+
+// export default DashboardPage
